9bcfcf34017a8410c85b2b745e750c38e883da58
Andersonmendes2016 <lge500meu@gmail.com>
2017-12-19 20:48:01 -0200

    condor: Pissel Bringup

--- aosp_condor.mk
+++ aosp_condor.mk
@@ -1,10 +1,13 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+TARGET_ARCH := arm
+TARGET_DENSITY := hdpi
+TARGET_BOOT_ANIMATION_RES := 720
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_condor
+PRODUCT_NAME := aosp_condor
 PRODUCT_RELEASE_NAME := MOTO E
 PRODUCT_MODEL := Moto E
