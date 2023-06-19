04943b018b33a2be473f7b7175ce44c7bf87ef6a
opsonc <58789337+opsonc@users.noreply.github.com>
2023-06-19 12:28:05 +0300

    Update gzosp.mk

--- gzosp.mk
+++ gzosp.mk
@@ -5,6 +5,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 $(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := gzosp_condor
-PRODUCT_RELEASE_NAME := MOTO E
-PRODUCT_MODEL := Moto E
+PRODUCT_NAME := gzosp_chakra
+PRODUCT_RELEASE_NAME := LUMIA 535
+PRODUCT_MODEL := Lumia 535
